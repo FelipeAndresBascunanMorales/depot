29636e01bd0bbc1cb7a7684705ba2effeea5a26d
FelipeAndresBascunanMorales <f.bascunan03@gmail.com>
2024-07-17 21:13:53 -0400

    feat: add cache for product and another little cosmetic things

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,8 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'h1', 'Welcome to the Store'
+    assert_select '.product', 3
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 end
